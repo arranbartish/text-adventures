6fd707cee9bd3e956c69e050ad38a4d01179c46a
Arran Bartish <abartish@teksystems.com>
2018-04-15 19:01:08 -0400

    dark duck use general components

--- TheDarkDuck/src/main/java/au/bartish/game/HandBag.java
+++ TheDarkDuck/src/main/java/au/bartish/game/HandBag.java
@@ -1,5 +1,16 @@
 package au.bartish.game;
 
-public class HandBag extends BaseItemContainer {
+import static org.apache.commons.lang3.ArrayUtils.add;
 
+public class HandBag extends Inventory {
+
+    @Override
+    public String[] listInventoryCommands() {
+        return add(super.listInventoryCommands(), "handbag");
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "handbag";
+    }
 }
--- TheDarkDuck/src/main/java/au/bartish/game/TheDarkDuck.java
+++ TheDarkDuck/src/main/java/au/bartish/game/TheDarkDuck.java
@@ -1,13 +1,11 @@
 package au.bartish.game;
 
-import org.apache.commons.collections.CollectionUtils;
-
 import java.io.PrintStream;
-import java.util.Collection;
 import java.util.Scanner;
 
-public class TheDarkDuck
-{
+import static au.bartish.game.Artifact.DEFAULT;
+
+public class TheDarkDuck extends GameTick<Artifact> implements Game {
 
     private final Scanner scanner;
     private final PrintStream out;
@@ -16,6 +14,7 @@ public class TheDarkDuck
     private Location currentLocation = world.get("sky");
 
     public TheDarkDuck(Scanner scanner, PrintStream out) {
+        super(DEFAULT, scanner, out);
         this.scanner = scanner;
         this.out = out;
     }
@@ -24,42 +23,33 @@ public class TheDarkDuck
         out.println("Welcome to the Dark of Darkness!\n");
     }
 
-    public void tick() {
-        out.println(currentLocation.getStory());
-        out.println(currentLocation.getQuestion());
-        final String response = scanner.nextLine();
-        globalActionHandler(response, currentLocation, handBag);
-        currentLocation = currentLocation.doAction(response);
+    @Override
+    public Location getCurrentLocation() {
+        return currentLocation;
     }
 
+    @Override
+    public void updateLocation(Location newLocation) {
+        currentLocation = newLocation;
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return handBag;
+    }
 
     public static void main( String[] args ) {
+
+        Scanner scanner = new Scanner(System.in);
         TheDarkDuck theDarkDuck = new TheDarkDuck(
-                new Scanner(System.in),
+                scanner,
                 System.out);
-        theDarkDuck.welcome();
+        LoopingGame<Artifact> loop = new LoopingGame<>(
+                DEFAULT,
+                theDarkDuck,
+                scanner,
+                System.out);
 
-        while (true) {
-            theDarkDuck.tick();
-        }
+        loop.execute();
     }
-
-    public static Artifact whichItem() {
-        return Artifact.SWORD;
-    }
-
-    public static void globalActionHandler(String action, Location location, ItemContainer backpack) {
-        final ItemMover itemMover = new ItemMover();
-        final String lowerAction = action.toLowerCase();
-        if (lowerAction.startsWith("take")) {
-            Collection<Artifact> items = Artifact.DEFAULT.find(lowerAction.replaceAll("take ", ""));
-            Item item = ((Artifact) CollectionUtils.get(items, 0)).get();
-            if (!itemMover.moveItem(item, location, backpack)){
-                System.out.println(item + " is not in the " + location.getDisplayName());
-            }
-        } else if (action.equalsIgnoreCase("backpack")) {
-            System.out.println("your backpack " + ((backpack.isEmpty())? "has nothing in it": "contains:"+backpack.listItems()));
-        }
-    }
-
 }
